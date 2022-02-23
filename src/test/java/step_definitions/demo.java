b4a3cd8337ff6dbd42295e5bed5d6f32241a26d4
Owner <Owner@192.168.0.11>
2022-02-23 12:18:28 -0500

    firstcode framework Florence

--- src/test/java/step_definitions/demo.java
+++ src/test/java/step_definitions/demo.java
@@ -1,29 +1,26 @@
-package stepdefinition;
+package step_definitions;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import base.MainClass;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
-public class demo {
+public class demo extends MainClass {
 	WebDriver driver;
-	//"C:\Users\Owner\Desktop\drivers\chromedriver.exe"
-	public static void main(String[] args) {
-		TODO Auto-generated method stub
+	
+	
+		
 	
 @Given("I want to write a step with precondition")
 public void i_want_to_write_a_step_with_precondition() {
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Owner\\Desktop\\drivers\\chromedriver.exe"); 
-	WebDriver driver = new ChromeDriver();
-	driver.get("https://www.amazon.com");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	
    }
 
 @When("I complete action")
